4017850b159cb6d89e2429e30c787b3d6768e06c
Jiří Doleček <jiri.dolecek@wedol.cz>
2026-02-15 17:06:36 +0100

    Add core library and test app for SteelSeries integration
    Introduce Wedol.SteelSeries core library with SteelSeriesGG class, providing access to the SteelSeries program data path. Add Wedol.SteelSeries.TestApp as a console application to demonstrate usage. Move packaging metadata from Sonar to the new core project and update the solution to include both new projects.

---
+++ Wedol.SteelSeries.TestApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Wedol.SteelSeries.TestApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        SteelSeriesGG ssgg = new SteelSeriesGG();
+
+        Console.WriteLine(ssgg.SteelSeriesGGProgramDataPath);
+    }
+}
---
+++ Wedol.SteelSeries/SteelSeriesGG.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Wedol.SteelSeries
+{
+    public sealed class SteelSeriesGG
+    {
+        public string SteelSeriesGGProgramDataPath { get; private set; }
+
+        public SteelSeriesGG()
+        {
+            SteelSeriesGGProgramDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "SteelSeries");
+        }
+
+        public SteelSeriesGG(string SteelSeriesGGProgramDataPath)
+        {
+            this.SteelSeriesGGProgramDataPath = SteelSeriesGGProgramDataPath;
+        }
+    }
+}
